207533f4d4277686e729db57612bbd4cfc2679af
Oh seung min <ohmin@gmail.com>
2024-08-01 10:35:17 +0900

    express.static을 사용해서 정적 파일을 css, javascript 파일을 읽음

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
